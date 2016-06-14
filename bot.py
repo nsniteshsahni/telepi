0f978cf71a6507359d6253111f7d14d13268b6d1
Nitesh Sahni <nsniteshsahni.007@gmail.com>
2016-06-14 11:26:58 +0530

    Comments added

--- bot.py
+++ bot.py
@@ -6,19 +6,19 @@ import telepot
 import wikipedia
 import json
 
-api = '6d93d0e4608c561b117bcc41a860d938'
+api = '6d93d0e4608c561b117bcc41a860d938'  # API Key for OpenWaetherMap 
 owm = pyowm.OWM(api)
-forecast = owm.daily_forecast("Delhi,in")
+forecast = owm.daily_forecast("Delhi,in") # Weather forecast details
 tomorrow = pyowm.timeutils.tomorrow()
 forecast.will_be_sunny_at(tomorrow)
-observation = owm.weather_at_place('Delhi,in')
-w = observation.get_weather()
+observation = owm.weather_at_place('Delhi,in')  #Hardcoding the weather place to be delhi,Soon add functionality to autodetect location
+w = observation.get_weather() 
 def handle(msg):
-    chat_id = msg['chat']['id']
-    command = msg['text']
+    chat_id = msg['chat']['id']  # Stores chat id for referencing message
+    command = msg['text'] # Filters text from the message sent
 
     print ('Got command: %s' % command)
-
+   # Here Starts the command interpretation
     if command == '/roll':
         bot.sendMessage(chat_id, random.randint(1,6))
     elif command == '/time':
@@ -34,8 +34,11 @@ def handle(msg):
         bot.sendMessage(chat_id,"List of supported commands is \n/hi - Greet Your Device\n/roll - Rolls a dice\n/weather - Tells detailed current weather report of Raspberry Pi's location\n/time - Tells current date and time\n/wiki <Topic Name> - Does a topic search on wikipedia and gives a summary of the topic.Try /wiki <Topic Name>\n\nSee your autofill for quick selection of command or tap '/' icon on right side of your chat textbox")
     else :
         bot.sendMessage(chat_id,"Type /help for list of supported commands till now,There are many more to come!!")
+
+# Here is the Telegram Bot API key
+
 bot = telepot.Bot('228758380:AAELxfbRv-nlJXCV_L9hP6zEHwAFf1bm040')
-bot.message_loop(handle)
+bot.message_loop(handle) # Calling bot and keeping it active infinitely
 print ('I am listening ...')
 
 while 1:
